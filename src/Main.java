623773286bba320baac25bc5d7440937c009019c
SachinthaAbeygunasekara <112684802+SachinthaAbeygunasekara@users.noreply.github.com>
2025-11-13 11:00:49 +0530

    feat(core): create main entry point to run the program

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import controller.ChatController;
+import model.User;
+import view.ChatWindow;
 
-/**
- *
- * @author Sachintha
- */
 public class Main {
-    
+    public static void main(String[] args) {
+        ChatController chatController = new ChatController();
+        chatController.registerWindow(new ChatWindow(chatController, new User("Sender 1")));
+    } 
 }
